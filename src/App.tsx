f20585f0836e61d9a7825c6bffc3d4ceef803481
Manu Benitez <mjbenitezgomez@gmail.com>
2018-11-04 00:07:38 +0100

    Log in

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,30 @@
-import * as React from 'react';
 import './App.css';
 
-import Header from './components/Header'
-import Intro from './components/Intro'
+import * as React from 'react';
+
+import Button from './components/Button'
+import Card from './components/Card'
+import Center from './components/Center'
+import Container from './components/Container'
+import Input from './components/Input'
+import Link from './components/Link'
+import Title from './components/Title'
+
 
 class App extends React.Component {
   public render() {
     return (
-      <div className="App">
-      <Header />
-      <Intro/>
-      <Intro/>
-      </div>
+      <Container>
+        <Card>
+          <Title>Iniciar Sesión</Title>
+          <Input placeholder="Correo" label='Correo'/>
+          <Input placeholder="Contraseña" label='Contraseña' />
+          <Button block={true}>Enviar</Button>
+          <Center>
+            <Link>Ir al registro</Link>
+          </Center>
+        </Card>
+      </Container>
     );
   }
 }
