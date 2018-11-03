255f472d546bc0d66f6264dd933216785af4cf70
Manu Benitez <mjbenitezgomez@gmail.com>
2018-11-03 14:54:29 +0100

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import './App.css';
+
+import Header from './components/Header'
+import Intro from './components/Intro'
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="App">
+      <Header />
+      <Intro/>
+      <Intro/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Intro.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+
+
+export default class Intro extends React.Component {
+    public render(){
+        return (
+            <p className="App-intro">
+                <span>Manolito</span>
+            </p>
+        );
+    }
+}
