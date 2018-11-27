187e0514d0de32ce57844247493937398330bce7
Manu Benitez <mjbenitezgomez@gmail.com>
2018-11-27 19:30:23 +0100

    create Posts

--- src/ducks/Posts.ts
+++ src/ducks/Posts.ts
@@ -1,10 +1,12 @@
-import { firestore } from 'firebase';
+import {  firestore  } from 'firebase';
 import { AnyAction, Dispatch  } from 'redux'
 import { IServices } from '../services';
+import { download } from '../utils';
 
 const START = 'posts/fetch-start'
 const SUCCESS = 'posts/fetch-success'
 const ERROR = 'posts/fetch-error'
+const ADD = 'posts/add'
 
 
 export interface IDataPosts{
@@ -27,6 +29,11 @@ const fetchSuccess = (payload: IDataPosts) => ({
 const fetchError = (error: Error) => ({
     error,
     type: ERROR,
+})
+
+const add = (payload: IDataPosts) =>({
+    payload,
+    type: ADD,
 })
 
 const initialState ={
@@ -55,6 +62,14 @@ export default function reducer (state = initialState, action: AnyAction) {
                 error: action.error,
                 fetching: false,
             }
+            case ADD:
+                return{
+                ...state,
+                data:{
+                    ...state.data,
+                    ...action.payload,
+                }
+                }
             default:
                 return state
     }
@@ -91,12 +106,42 @@ export const fetchPosts = () =>
 }
 
 export const like = (id: string) =>
-    async (dispatch: Dispatch, getState: () => any, {}: IServices) => {
-        // tslint:disable-next-line:no-console
-        console.log(id)
+    async (dispatch: Dispatch, getState: () => any, { auth }: IServices) => {
+        
+        if(!auth.currentUser){
+            return
+        }
+
+        const token = await auth.currentUser.getIdToken()
+        await fetch(`/api/posts/${id}/like`, {
+            headers: {
+                authorization: token
+            }
+        })
     }
 export const share = (id: string) =>
-    async (dispatch: Dispatch, getState: () => any, {}: IServices) => {
-        // tslint:disable-next-line:no-console
-        console.log(id)
+    async (dispatch: Dispatch, getState: () => any, { auth, db, storage}: IServices) => {
+        if(!auth.currentUser){
+            return
+        }
+
+        const token = await auth.currentUser.getIdToken()
+        const result = await fetch(`/api/posts/${id}/like`, {
+            headers: {
+                authorization: token
+            }
+        })
+        const url = await storage.ref(`posts/${id}.jpg`).getDownloadURL()
+        const blob = await download(url)
+        const { id: postId }: {id:string} = await result.json()
+        const ref = storage.ref(`posts/${postId}.jpg`)
+        await ref.put(blob)
+        const imageURL = ref.getDownloadURL()
+        const snap = await db.collection('posts').doc(postId).get()
+
+        dispatch(add({ [snap.id]: {
+            ...snap.data(),
+            imageURL,
+         } } as IDataPosts))
+
     }
